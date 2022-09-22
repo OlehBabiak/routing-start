baa92857eedab74d8ccdc4edcc4f8d74c46526b7
Oleh <oleg_bob@ukr.net>
2022-09-22 16:04:16 +0300

    Controlling Navigation with canDeactivate

--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-page-not-found',
@@ -10,6 +11,7 @@ export class PageNotFoundComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }
--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,41 +1,61 @@
 import {Component, OnInit} from '@angular/core';
 
 import {ServersService} from '../servers.service';
-import {ActivatedRoute, Params} from '@angular/router';
+import {ActivatedRoute, Params, Router} from '@angular/router';
+import {CanComponentDeactivate} from './can-deactivate-guard.service';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-edit-server',
   templateUrl: './edit-server.component.html',
   styleUrls: ['./edit-server.component.css']
 })
-export class EditServerComponent implements OnInit {
+export class EditServerComponent implements OnInit, CanComponentDeactivate {
   server: { id: number, name: string, status: string };
   serverName = '';
   serverStatus = '';
   allowEdit = false;
+  changesSaved = false;
 
   constructor(private serversService: ServersService,
-              private route: ActivatedRoute) {
+              private route: ActivatedRoute,
+              private router: Router
+  ) {
   }
 
   ngOnInit() {
-    // console.log('params', this.route.snapshot.queryParams);
-    // console.log('fragm', this.route.snapshot.fragment);
+    console.log('queryParams', this.route.snapshot.queryParams);
+    console.log('fragm', this.route.snapshot.fragment);
+    console.log('params', this.route.snapshot.params);
     this.route.queryParams
       .subscribe((qParams: Params) => {
-        console.log(qParams);
         this.allowEdit = qParams['allowEdit'] === '1';
       });
     this.route.params.subscribe((params: Params) => {
       this.server = this.serversService.getServer(+params['id']);
     });
-    // this.route.fragment.subscribe(value => console.log(value));
+    this.route.fragment.subscribe(value => console.log(value));
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
+    !this.allowEdit && setTimeout(() => {
+        this.router.navigate(['../'], {relativeTo: this.route});
+    }, 2000);
   }
 
   onUpdateServer() {
     this.serversService.updateServer(this.server.id, {name: this.serverName, status: this.serverStatus});
+    this.changesSaved = true;
+    this.router.navigate(['../'], {relativeTo: this.route});
   }
 
+  canDeactivateMeth(): Observable<boolean> | Promise<boolean> | boolean {
+    if (!this.allowEdit) {
+      return true;
+    }
+    if ((this.serverName !== this.server.name || this.serverStatus !== this.server.status) && !this.changesSaved) {
+      return confirm('Do you want to discard the changes?');
+    } else {
+      return true;
+    }
+  }
 }
